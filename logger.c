f41d17782719005851e05a76149e3845016a37ad
Aleksandr Rokashevich <rokashevich@gmail.com>
2021-02-20 10:55:30 +0300

    сократил timestamp чтобы короче логгируемая строка была: итак надо пихать в неё очень много информации

--- logger.c
+++ logger.c
@@ -37,12 +37,9 @@ void printer(const char* suffix, char* format, va_list arg) {
   struct timespec ts;
   clock_gettime(CLOCK_REALTIME, &ts);
   struct tm* ptm = localtime(&ts.tv_sec);
-  int tenths_ms = ts.tv_nsec / 1000000L;
 
-  // "[2021-02-05 17:31:35.101] "
-  fprintf(stdout, "[%04d-%02d-%02d %02d:%02d:%02d.%03d]", 1900 + ptm->tm_year,
-          ptm->tm_mon + 1, ptm->tm_mday, ptm->tm_hour, ptm->tm_min, ptm->tm_sec,
-          tenths_ms);
+  fprintf(stdout, "%d%02d%02d%02d%02d%02d", ptm->tm_year - 100, ptm->tm_mon + 1,
+          ptm->tm_mday, ptm->tm_hour, ptm->tm_min, ptm->tm_sec);
   fprintf(stdout, "%s", suffix);
 
   char* traverse = format;
--- tests/start_some_apps.sh
+++ tests/start_some_apps.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
+curl http://localhost:14157/reset
+curl -X POST http://localhost:14157/setup -d "env=\
+DISPLAY=$DISPLAY \
+XAUTHORITY=$XAUTHORITY \
+PATH=/opt/st/simulator/bin:/opt/st/simulator/share/postgresql/pgsql/bin:$PATH \
+LD_LIBRARY_PATH=/opt/st/simulator/bin:/opt/st/simulator/lib:/opt/st/simulator/share/postgresql/pgsql/lib:$LD_LIBRARY_PATH"
+
 #curl -X POST http://localhost:14157/watch -d "env=DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY LD_LIBRARY_PATH=.&cmd=xterm -e mc"
 #curl -X POST http://localhost:14157/watch -d "pwd=/bad&env=DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY LD_LIBRARY_PATH=.&cmd=xterm"
 #curl -X POST http://localhost:14157/watch -d "pwd=`pwd`&cmd=bash generator_stdout_stderr.sh"
 #curl -X POST http://localhost:14157/watch -d "pwd=`pwd`&cmd=./segfault"
 #curl -X POST http://localhost:14157/watch -d "env=DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY LD_LIBRARY_PATH=.&cmd=glxgears"
 #curl -X POST http://localhost:14157/watch -d "env=A=a B=b C=c&cmd=glxgears"
-#curl -X POST http://localhost:14157/watch -d "cmd=glxgears"
+curl -X POST http://localhost:14157/watch -d "cmd=/bin/bash /opt/rokashevich/dog/tests/env.sh"
 
-curl -X POST http://localhost:14157/setup -d "env=C=c CC=cc LD_LIBRARY_PATH=1:2:3"
-curl -X POST http://localhost:14157/watch -d 'env=P=p P=pp LD_LIBRARY_PATH=$LD_LIBRARY_PATH:4:5:6:$a:$bb:$cc X=x&pwd=/opt/rokashevich/dog/tests&cmd=/bin/bash env.sh'
+#curl -X POST http://localhost:14157/watch -d 'env=PATH=$PATH:/tmp/bin LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/tmp/lib&pwd=/opt/rokashevich/dog/tests&cmd=/bin/bash ./env.sh'
 
