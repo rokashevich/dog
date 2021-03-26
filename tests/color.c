13d96c481f91bc4f96019652d0c934180477ffa8
Aleksandr Rokashevich <rokashevich@gmail.com>
2021-03-26 10:56:20 +0300

    Пример разноцветного вывода в printf

---
+++ tests/color.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main() {
+  fprintf(stderr, "\033[0;30m");  // Чёрный.
+  fprintf(stderr, "Black\n");
+
+  fprintf(stderr, "\033[0;31m");  // Красный.
+  fprintf(stderr, "Red\n");
+
+  fprintf(stderr, "\033[0;32m");  // Зелёный.
+  fprintf(stderr, "Green\n");
+
+  fprintf(stderr, "\033[0;33m");  // Жёлтый
+  fprintf(stderr, "Yellow\n");
+
+  fprintf(stderr, "\033[0;34m");  // Синий.
+  fprintf(stderr, "Blue\n");
+
+  fprintf(stderr, "\033[0;35m");  // Фиолетовый.
+  fprintf(stderr, "Purple\n");
+
+  fprintf(stderr, "\033[0;36m");  // Голубой.
+  fprintf(stderr, "Cyan\n");
+
+  fprintf(stderr, "\033[0;37m");  // Белый.
+  fprintf(stderr, "White\n");
+
+  fprintf(stderr, "\033[0m");  // Дефолтный.
+  fprintf(stderr, "Default\n");
+}
