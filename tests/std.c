3ce3307a51469e9f8e820d41775290d73343ac75
Aleksandr Rokashevich <rokashevich@gmail.com>
2021-04-30 15:45:23 +0300

    Сборка тестов через cmake

--- tests/std.c
+++ tests/std.c
@@ -1,11 +1,38 @@
 #include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static void *out() {
+  for (int i = 0; i < 1000000; ++i) {
+    fprintf(stdout, "o");
+  }
+  return strdup("stdout finish");
+}
+
+static void *err() {
+  for (int i = 0; i < 1000000; ++i) {
+    fprintf(stderr, "e");
+  }
+  return strdup("stderr finish");
+}
 
 int main() {
-  for (int i = 1; i <= 100; ++i) {
-    fprintf(stderr, "abcdefghijklmnopqrstuwxyz%d\n", i);
-    // fprintf(stdout, "TEXT TEXT TEXT TEXT TEXT TEXT TEXT STDOUT %d\n",i);
-    // fprintf(stderr, "TEXT TEXT TEXT TEXT TEXT TEXT TEXT STDERR %d\n",i);
-  }
-  return 1 / 0;
+  pthread_t out_id;
+  pthread_t err_id;
+  pthread_create(&out_id, NULL, &out, NULL);
+  pthread_create(&err_id, NULL, &err, NULL);
+
+  int s;
+  void *res;
+
+  s = pthread_join(out_id, &res);
+  fprintf(stdout, "out: s=%d res=%s", s, (char *)res);
+  free(res);
+
+  s = pthread_join(err_id, &res);
+  fprintf(stderr, "err: s=%d res=%s", s, (char *)res);
+  free(res);
+
+  return 0;
 }
